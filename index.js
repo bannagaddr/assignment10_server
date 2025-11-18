c8e4da6a1e4705bfcdea411d2f982103c7c6690c
bannagaddr <bannamaddr@gmail.com>
2025-11-18 09:33:12 +0600

    card delete function implement here

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 3000;
 
 // cors middleware for allow all cross request
@@ -34,26 +34,64 @@ async function run() {
     const cropsCollection = assignment10.collection("crops");
 
     // api for getting data
-    // get method: find(), findOne()
     app.get("/", (req, res) => {
       res.send("");
     });
 
-    // function for find data
+    // api for getting data
+    // get method: find(), findOne()
     app.get("/crops", async (req, res) => {
       const result = await cropsCollection.find().toArray();
+
+      // send req data to client side
+      res.send(result);
+    });
+
+    // get data by id from mongodb
+    app.get("/crops/:id", async (req, res) => {
+      const { id } = req.params;
+      // const objectId = new ObjectId(id) (another option)
+      console.log(id);
+
+      const result = await cropsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // Express.js
+    app.get("/my-post/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await interestsCollection
+        .find({ userEmail: email })
+        .toArray();
       res.send(result);
     });
 
     // put data to mongodb
     // post method: insertOne(), insertMany()
     app.post("/crops", async (req, res) => {
-      // data from client side
+      // data from client side and set to body
       const data = req.body;
-      console.log(data);
+      // console.log(data);
 
-      // data insert to database
-      const result = await cropsCollection.insertOne();
+      // data insert into the database
+      const result = await cropsCollection.insertOne(data);
+
+      // send req data to client side
+      res.send(result);
+    });
+
+    // updated data from mongodb
+    // update method: updateOne(), updateMany()
+
+    // delete data from mongodb
+    // delete method: deleteOne(), deleteMany()
+    app.delete("/crops/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+
+      const result = await cropsCollection.deleteOne(filter);
+
       res.send(result);
     });
 
